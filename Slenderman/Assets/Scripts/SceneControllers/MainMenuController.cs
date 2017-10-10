67d8a3d74bb383535df9082244b5c0cbd9e18b03
Ut0pi4 <xiangyao_ng95@hotmail.com>
2017-10-10 17:01:49 +1100

    Initial

---
+++ Slenderman/Assets/Scripts/SceneControllers/MainMenuController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+    
+    public void StartGame()
+    {
+		SceneManager.LoadScene ("Game");
+    }
+
+    public void OpenInstructions()
+    {
+		SceneManager.LoadScene ("Instructions");
+    }
+
+    public void OpenOptions()
+    {
+		SceneManager.LoadScene ("Options");
+    }
+
+	public void ExitGame()
+	{
+		Application.Quit ();
+	}
+}
